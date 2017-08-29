910f021b002a9c511e8d7a1452e2aa0cdd4f0400
eshankuthu <kuthu.eshan@gmail.com>
2017-08-29 17:46:13 -0500

    UserFront Added with domain

---
+++ UserFront/src/main/java/com/userfront/controller/HomeController.java
@@ -0,0 +1,56 @@
+package com.userfront.controller;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.userfront.domain.User;
+
+@Controller
+public class HomeController {
+
+	
+	@RequestMapping("/")
+	public String home(){
+		return "redirect:/index";
+	}
+	
+	@RequestMapping("/index")
+	public String index(){
+		return "index";
+	}
+	
+	@RequestMapping(value="/signup", method = RequestMethod.GET)
+	public String signup(Model model){
+		User user = new User();
+		model.addAttribute("user", user);
+		return "signup";
+	}
+	
+	@RequestMapping(value="/signup", method = RequestMethod.POST)
+	public void signupPost(@ModelAttribute("user") User user, Model model){
+		/*if(userService.checkUserExists(user.getUsername(),user.getEmail())){
+			if(userService.checkEmailExists(user.getEmail())){
+				model.addAttribute("emailExists",true);
+			}
+			if(userService.checkUsernameExists(user.getUsername())){
+				model.addAttribute("usernameExists",true);
+			}
+			return "signup";
+		}else{
+			Set<UserRole> userRoles = new HashSet<>();
+			userRoles.add(new UserRole(user,roleDao.findByName("USER")));
+			userSerivce.createUser(user,userRoles); 
+			
+			return "redirect:/";
+			
+		}*/
+	}
+	
+	
+}
+	
