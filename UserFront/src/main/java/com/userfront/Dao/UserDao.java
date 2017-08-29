d96782aaf580d170e7edf2175972f21aadd49c82
eshankuthu <kuthu.eshan@gmail.com>
2017-08-29 18:14:33 -0500

    Service and Dao Added for checking Username and Email exists

---
+++ UserFront/src/main/java/com/userfront/Dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.userfront.Dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.userfront.domain.User;
+
+public interface UserDao extends CrudRepository<User, Long> {
+
+	User findByUsername(String username);
+	User findByEmail(String email);
+}
--- UserFront/src/main/java/com/userfront/controller/HomeController.java
+++ UserFront/src/main/java/com/userfront/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.userfront.controller;
 
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,10 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.userfront.domain.User;
+import com.userfront.service.UserService;
 
 @Controller
 public class HomeController {
 
+	
+	@Autowired
+	UserService userService;
 	
 	@RequestMapping("/")
 	public String home(){
@@ -32,9 +37,9 @@ public class HomeController {
 	}
 	
 	@RequestMapping(value="/signup", method = RequestMethod.POST)
-	public void signupPost(@ModelAttribute("user") User user, Model model){
-		/*if(userService.checkUserExists(user.getUsername(),user.getEmail())){
-			if(userService.checkEmailExists(user.getEmail())){
+	public String signupPost(@ModelAttribute("user") User user, Model model){
+		if(userService.checkUserExists(user.getUsername(), user.getEmail())){
+				if(userService.checkEmailExists(user.getEmail())){
 				model.addAttribute("emailExists",true);
 			}
 			if(userService.checkUsernameExists(user.getUsername())){
@@ -42,13 +47,11 @@ public class HomeController {
 			}
 			return "signup";
 		}else{
-			Set<UserRole> userRoles = new HashSet<>();
-			userRoles.add(new UserRole(user,roleDao.findByName("USER")));
-			userSerivce.createUser(user,userRoles); 
+			 userService.save(user);
 			
 			return "redirect:/";
-			
-		}*/
+		
+		}
 	}
 	
 	
